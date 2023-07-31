32e1e34021c922fd247f7ec8226ed953d2501ffa
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-07-31 16:03:38 -0300

    att

--- src/app/pages/movie/page.jsx
+++ src/app/pages/movie/page.jsx
@@ -1,12 +1,34 @@
 'use client'
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import MovieCard from '@/components/MovieCard';
 
-import ( useState )
-
-import React, { useState } from 'react'
+const moviesURL = "https://api.themoviedb.org/3/movie/";
+const apiKey =  "590fe327d68ab66718c93cde9cecca3b";
 
 const Movie = () => {
+
+  const {id} = useParams()
+  const [movie, setmovie] = useState(null)
+
+  const getMovie = async(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setmovie(data);
+  }
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?api_key=${apiKey}`
+    getMovie(movieUrl)
+  })
+
   return (
-    <div>Movie</div>
+    <div>
+      {movie && (
+        <>{movie.title}</>
+      )}
+    </div>
   )
 }
 
